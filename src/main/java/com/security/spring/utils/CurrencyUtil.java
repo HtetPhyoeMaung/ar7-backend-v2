f67ba2129780c2156a83c7d0167b791dc21ea238
Venzee24 <htetphyoemaung3363@gmail.com>
2025-08-03 00:01:25 +0630

    bugFIx  : add IDR currency

--- src/main/java/com/security/spring/utils/CurrencyUtil.java
+++ src/main/java/com/security/spring/utils/CurrencyUtil.java
@@ -8,7 +8,7 @@ public class CurrencyUtil {
     }
     public static BigDecimal getCurrencyRate(String currencyCode) {
         return switch (currencyCode) {
-            case "IDR2", "IRR2", "KHR2", "KRW2", "LAK2", "LBP2", "MMK2", "UZS2", "VND2", "COP2", "PYG2" -> new BigDecimal("0.001");
+            case "IDR", "IRR2", "KHR2", "KRW2", "LAK2", "LBP2", "MMK2", "UZS2", "VND2", "COP2", "PYG2" -> new BigDecimal("0.001");
             case "MMK" -> new BigDecimal("1");
             case "MMK3" -> new BigDecimal("0.01");
             default -> BigDecimal.ONE;

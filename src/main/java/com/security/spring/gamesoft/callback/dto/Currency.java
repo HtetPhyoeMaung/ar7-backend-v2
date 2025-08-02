312616a616603b004e225a69970f99be971d1685
Venzee24 <htetphyoemaung3363@gmail.com>
2025-08-03 01:12:18 +0630

    bugfi

--- src/main/java/com/security/spring/gamesoft/callback/dto/Currency.java
+++ src/main/java/com/security/spring/gamesoft/callback/dto/Currency.java
@@ -3,7 +3,9 @@ package com.security.spring.gamesoft.callback.dto;
 import java.math.BigDecimal;
 
 public enum Currency {
-    MMK(1), MMK2(0.001), IDR(0.001), IDR2(0.001), MMK3(0.01);
+    MMK(1), MMK2(0.001), IDR(0.001), IDR2(0.001),
+    KRW2(0.001), VND2(0.001), LAK2(0.001), KHR2(0.001),
+    MMK3(0.01);
 
     private final double rate;
 

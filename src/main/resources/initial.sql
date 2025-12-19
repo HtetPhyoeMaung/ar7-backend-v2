89b7712f18d6ae0d153a72255796d44e3469917e
HtetPhyoeMaung <maunghtetphyoe@gmail.com>
2025-12-19 22:35:31 +0630

    fix : update insert default value of game_bank_setting query

--- src/main/resources/initial.sql
+++ src/main/resources/initial.sql
@@ -1,2 +1,2 @@
-INSERT IGNORE INTO game_bank_setting (id, agent_code, call_back_url, password)
-VALUES (1, 'TEST', 'https://api.gamebank.vip', 'TEST-PW');
+INSERT IGNORE INTO game_bank_setting (id, agent_code, call_back_url, agent_id)
+VALUES (1, '86a6b626-e245-41b4-b167-6c873829219e', 'https://api.gamebank.vip', 'PTlGwa6V');
